63fc565b8b60569fb4e1e17512094634a6eee974
Ankur Agarwal <agarwalankur58@gmail.com>
2017-04-23 00:40:40 +0530

    Add basic sidebar to the editor

--- client/src/components/Editor/index.js
+++ client/src/components/Editor/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Button, Icon } from 'semantic-ui-react'
+import { Button, Icon, Sidebar, Segment, Menu, Header } from 'semantic-ui-react'
 
 export default class Editor extends Component {
   render () {
@@ -14,6 +14,31 @@ export default class Editor extends Component {
         </div>
 
         {/* Sidebar */}
+        <div className="c-editor__content">
+          <Sidebar.Pushable as={Segment} className="c-editor__content--all">
+            <Sidebar as={Menu} animation="push" width="thin" visible={true} icon="labeled" vertical inverted>
+              <Menu.Item name="home">
+                <Icon name="home" />
+                Home
+              </Menu.Item>
+              <Menu.Item name="gamepad">
+                <Icon name="gamepad" />
+                Games
+              </Menu.Item>
+              <Menu.Item name="camera">
+                <Icon name="camera" />
+                Channels
+              </Menu.Item>
+            </Sidebar>
+            <Sidebar.Pusher>
+              <Segment basic>
+                <Header as="h3">Application Content</Header>
+              </Segment>
+            </Sidebar.Pusher>
+          </Sidebar.Pushable>
+        </div>
+
+        {/* Footer */}
 
       </div>
     )
