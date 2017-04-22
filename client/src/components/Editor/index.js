caaba9d56835759a7198d249af13c169931ac9b9
Ankur Agarwal <agarwalankur58@gmail.com>
2017-04-23 02:44:30 +0530

    Add dummy menu item to sidebar

--- client/src/components/Editor/index.js
+++ client/src/components/Editor/index.js
@@ -16,19 +16,19 @@ export default class Editor extends Component {
         {/* Sidebar */}
         <div className="c-editor__content">
           <Sidebar.Pushable as={Segment} className="c-editor__content--all">
-            <Sidebar as={Menu} animation="push" width="thin" visible={true} icon="labeled" vertical inverted>
-              <Menu.Item name="home">
-                <Icon name="home" />
-                Home
-              </Menu.Item>
-              <Menu.Item name="gamepad">
-                <Icon name="gamepad" />
-                Games
-              </Menu.Item>
-              <Menu.Item name="camera">
-                <Icon name="camera" />
-                Channels
-              </Menu.Item>
+            <Sidebar as={Menu} animation="push" width="thin" visible={true} icon="labeled" vertical inverted className="c-sidebar">
+              <Menu.Item name="Overview" className="c-sidebar__menu-item"/>
+              <Menu.Item name="1. Early Life" className="c-sidebar__menu-item"/>
+              <Menu.Item name="childhood" className="c-sidebar__submenu-item">1.1 Early Childhood</Menu.Item>
+              <Menu.Item name="university" className="c-sidebar__submenu-item">1.2 University</Menu.Item>
+
+              <Menu.Item name="1. Early Life" className="c-sidebar__menu-item"/>
+              <Menu.Item name="childhood" className="c-sidebar__submenu-item">1.1 Early Childhood</Menu.Item>
+              <Menu.Item name="university" className="c-sidebar__submenu-item">1.2 University</Menu.Item>
+
+              <Menu.Item name="1. Early Life" className="c-sidebar__menu-item"/>
+              <Menu.Item name="childhood" className="c-sidebar__submenu-item">1.1 Early Childhood</Menu.Item>
+              <Menu.Item name="university" className="c-sidebar__submenu-item">1.2 University</Menu.Item>
             </Sidebar>
             <Sidebar.Pusher>
               <Segment basic>
