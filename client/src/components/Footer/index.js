7bc98bbbdf5ee4d21d6261eb54e82b08dc77e907
Ankur Agarwal <agarwalankur58@gmail.com>
2017-06-04 09:48:52 +0530

    footer on specific pages

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,13 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import { withRouter, Link } from 'react-router-dom'
 
-export default class Footer extends Component {
+class Footer extends Component {
   render () {
-    return (
-      <footer className="c-app-footer">
-        <p className="c-app-footer__top-line">Sum of all human knowledge in multi-media format, by human beings, for human beings.</p>
-        <p className="c-app-footer__middle-line">Text is available under the Creative Commons Attribution-ShareAlike License; Users retain ownership of images and videos uploaded by them.</p>
-        <p className="c-app-footer__bottom-line">By using this site, you agree to the Terms of Use and Privacy Policy.</p>
-      </footer>
-    )
+    const { location: { pathname } } = this.props
+
+    return (pathname === '/' ||
+      pathname === '/login' ||
+      pathname === '/signup') ? (
+        <footer className="c-app-footer">
+          <p className="c-app-footer__top-line">Sum of all human knowledge in multi-media format, by human beings, for human beings.</p>
+          <p className="c-app-footer__middle-line">Text is available under the Creative Commons Attribution-ShareAlike License; Users retain ownership of images and videos uploaded by them.</p>
+          <p className="c-app-footer__bottom-line">By using this site, you agree to the Terms of Use and Privacy Policy.</p>
+
+          <div className="c-app-footer__actions">
+            <span className="c-app-footer__about">
+              <Link to="/about" className="c-app-footer__link">
+                About Us
+              </Link>
+            </span>
+
+            <span className="c-app-footer__contact">
+              Contact Us
+            </span>
+          </div>
+        </footer>
+    ) : null
   }
 }
+
+Footer.propTypes = {
+  location: PropTypes.object.isRequired,
+}
+
+export default withRouter(Footer)
