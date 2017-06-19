a99c0db2cb842d5ffc633cb247f81bd026deab23
Ankur Agarwal <agarwalankur58@gmail.com>
2017-06-20 00:09:52 +0530

    change db

--- server/config/index.js
+++ server/config/index.js
@@ -1,6 +1,6 @@
 module.exports = {
   secret: process.env.APP_SECRET,
-  db: 'mongodb://127.0.0.1:27017/videowiki',
+  db: process.env.DB_CONNECTION_URL,
   mail: {
     transportOptions: {
       service: 'gmail',
