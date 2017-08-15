b765f6ac2e31afe5c47fd92434dca0201ecc491c
Hassan Amin <hassan94924@gmail.com>
2017-08-15 13:21:49 +0200

    Change to simple cached for loop for performance

--- server/bots/autoupdate/index.js
+++ server/bots/autoupdate/index.js
@@ -311,9 +311,9 @@ const getLatestData = function(title, callback){
         updatedSlides = addNewSlides(oldUpdatedSlides, addedSlidesArray);
 
         // recalculate the position attribute on the slides ;
-        updatedSlides.forEach( (slide, index) => {
-            slide.position = index;
-        })
+        for(var i = 0, len = updatedSlides.length; i<len; i++ ) {
+            updatedSlides[i].position = i;
+        }
         
         res.json({ updatedSlides, removedSlidesBatch, addedSlidesBatch, addedSlidesArray});
     })
