6702870106116da3c8ce289f95c7afe7af96094c
Ankur Agarwal <agarwalankur58@gmail.com>
2017-08-20 14:31:12 +0530

    default slide

--- client/src/components/Editor/Viewer/index.js
+++ client/src/components/Editor/Viewer/index.js
@@ -35,11 +35,11 @@ class Viewer extends Component {
 
   showItem (item, isActive) {
     const { media } = item
+    let component
 
     if (media) {
       const array = media.split('.')
       const format = array[array.length - 1]
-      let component
       if (isActive) {
         switch (format) {
           case 'mp4':
@@ -95,17 +95,26 @@ class Viewer extends Component {
             break
         }
       }
-
-      return isActive ? component
-        : (
-          <div className="outer-container">
-            <div className="inner-container">
-              <div className="overlay"/>
-              {component}
-            </div>
+    } else {
+      component = (
+        <div className="c-editor__content-video-viewer">
+          <div className="box__input">
+            <svg className="box__icon" xmlns="http://www.w3.org/2000/svg" width="50" height="43" viewBox="0 0 50 43"><path d="M48.4 26.5c-.9 0-1.7.7-1.7 1.7v11.6h-43.3v-11.6c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v13.2c0 .9.7 1.7 1.7 1.7h46.7c.9 0 1.7-.7 1.7-1.7v-13.2c0-1-.7-1.7-1.7-1.7zm-24.5 6.1c.3.3.8.5 1.2.5.4 0 .9-.2 1.2-.5l10-11.6c.7-.7.7-1.7 0-2.4s-1.7-.7-2.4 0l-7.1 8.3v-25.3c0-.9-.7-1.7-1.7-1.7s-1.7.7-1.7 1.7v25.3l-7.1-8.3c-.7-.7-1.7-.7-2.4 0s-.7 1.7 0 2.4l10 11.6z"/></svg>
+            <label>Choose a file or drag it here.</label>
           </div>
-        )
+        </div>
+      )
     }
+
+    return isActive ? component
+    : (
+      <div className="outer-container">
+        <div className="inner-container">
+          <div className="overlay"/>
+          {component}
+        </div>
+      </div>
+    )
   }
 
   renderItems () {
