e8f59bcfdd115bb051a5727e76252b3d2def01e3
Hassan Amin <hassan94924@gmail.com>
2017-08-23 04:48:06 +0200

    checkpoint

--- server/bots/autoupdate/init.js
+++ server/bots/autoupdate/init.js
@@ -1,3 +1,32 @@
 import { runBot } from './index';
+import { CronJob } from 'cron';
 
-runBot(3);
+const x = 2 ; // multiple by 24 hours 
+const NumberOfArticlesPerUpdate = 50;
+var runnedTimes = 0;
+
+var job = new CronJob({
+  cronTime: '00 30 00 * * *',
+  onTick: function() {
+    // if runnded times is less than x, dont execute
+    runnedTimes ++ ;
+    if(runnedTimes >= x ){
+
+        // run the script, clear running flag 
+        console.log('Auto update bot spawned! getting to work!')
+        runBot(NumberOfArticlesPerUpdate);
+        runnedTimes = 0;
+
+    } else {
+        console.log('dont execute');
+    }
+    /*
+     * Runs every weekday (Monday through Friday)
+     * at 11:30:00 AM. It does not run on Saturday
+     * or Sunday.
+     */
+  },
+  start: true,
+  timeZone: 'Asia/Kolkata'
+});
+job.start();
