ed41b865b3b7f87a0eaf63e8d60eef4088095786
hassanamin994 <hassanmamin994@gmail.com>
2018-05-09 02:34:10 +0200

    handle empty content on article summary

--- server/controllers/wiki/index.js
+++ server/controllers/wiki/index.js
@@ -216,7 +216,8 @@ const getArticleSummary = function(title, callback) {
         console.log(err)
         return callback(err);
       }
-      return callback(null, {image: image, articleText: articleText.substring(0, 200) });
+      let text = articleText ? articleText.substring(0, 200) : '';
+      return callback(null, {image: image, articleText: text });
     })
   }); 
 }
