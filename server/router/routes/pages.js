d6aaf352ea96951031923e7e4c116258bc9802d1
hassanamin994 <hassanmamin994@gmail.com>
2018-11-05 04:47:07 +0200

    update meta tags

--- server/router/routes/pages.js
+++ server/router/routes/pages.js
@@ -28,9 +28,10 @@ module.exports = () => {
             <meta itemprop="name" content="Videowiki: ${article.title.split('_').join(' ')}" >
             <meta itemprop="description" content="Checkout the new VideoWiki article at https://videowiki.org/videowiki/${article.title}?wikiSource=${article.wikiSource}">
             <meta itemprop="image" content="${article.image}">
-            <meta property="og:url" content="${location.origin}/videowiki/${article.title}" >
+            <meta property="og:url" content="${location.origin}/api/pages/article/${article.title}?wikiSource=${wikiSource}" >
             <meta property="og:type" content="article" >
             <meta property="og:image" content="${article.image}" >
+            <meta property="fb:app_id" content="314041545858819" />
             <meta property="og:title" content="Videowiki: ${article.title.split('_').join(' ')}" >
             <meta property="og:site_name" content="Videowiki" >
             <meta property="og:description" content="Checkout the new VideoWiki article at https://videowiki.org/videowiki/${article.title}?wikiSource=${article.wikiSource}" >
