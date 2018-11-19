ce6565b2ec0b8b43d5bac9bd170b5ae3de9e10fe
hassanamin994 <hassanmamin994@gmail.com>
2018-11-19 05:13:12 +0200

    Fix

--- client/src/components/common/AuthModal.js
+++ client/src/components/common/AuthModal.js
@@ -36,7 +36,7 @@ const AuthModal = (props) => (
         <p>A good chance to Log In</p>
       </Modal.Description>
     </Modal.Content>
-    <AuthButtons fluid onAuth={props.onClose} style={{ height: 50, borderRadius: 0 }} />
+    <AuthButtons fluid onAuth={props.onClose} noMargen style={{ height: 50, borderRadius: 0 }} />
   </Modal >
 )
 
