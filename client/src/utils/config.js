b1dfcefbd6476dab2370ac5ff315f0121efeee1a
hassanamin994 <hassanmamin994@gmail.com>
2019-05-07 04:57:26 +0200

    add hostname

--- client/src/utils/config.js
+++ client/src/utils/config.js
@@ -8,10 +8,10 @@ module.exports = {
     'fr': `${PROTOCOL}//localhost:4003`,
   },
   SOCKET_LANG_API_MAP: {
-    'en': `${PROTOCOL}//localhost:4000/en`,
-    'hi': `${PROTOCOL}//localhost:4001/hi`,
-    'es': `${PROTOCOL}//localhost:4002/es`,
-    'fr': `${PROTOCOL}//localhost:4003/fr`,
+    'en': `${PROTOCOL}//${window.location.hostname}:4000/en`,
+    'hi': `${PROTOCOL}//${window.location.hostname}:4001/hi`,
+    'es': `${PROTOCOL}//${window.location.hostname}:4002/es`,
+    'fr': `${PROTOCOL}//${window.location.hostname}:4003/fr`,
   },
   AVAILABLE_LANGUAGES: ['en', 'hi', 'es', 'fr'],
 }
