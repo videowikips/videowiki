8768d034dbabef62243ebebbdc08af40d48d79ba
hassanamin994 <hassanmamin994@gmail.com>
2019-06-16 20:49:19 +0200

    Update stop scripts

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const langs = ['en', 'hi'];
 const ports = [4000, 4001, 4002, 4003];
 
 langs.forEach((lang, index) => {
-  console.log(`pm2 start ./server/index.js --name videowiki-${lang} -- ${ports[index]} ${lang}`);
-  exec(`pm2 start ./server/index.js --name videowiki-${lang} -- ${ports[index]} ${lang}`, (err) => {
+  console.log(`pm2 start ./server/index.js --name videowiki-app-${lang} -- ${ports[index]} ${lang}`);
+  exec(`pm2 start ./server/index.js --name videowiki-app-${lang} -- ${ports[index]} ${lang}`, (err) => {
     if (err) {
       console.log('error initializing ', lang, ports[index], err);
     }
