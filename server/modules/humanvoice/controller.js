c0a5e612a8e59f2d8295fdda31613d25aedb0573
hassanamin994 <hassanmamin994@gmail.com>
2019-07-23 15:52:27 +0200

    fix

--- server/modules/humanvoice/controller.js
+++ server/modules/humanvoice/controller.js
@@ -17,7 +17,7 @@ const humanvoiceController = {
     if (!title || !wikiSource || !lang) {
       return res.status(400).send('title, wikiSource and lang are required');
     }
-    HumanVoiceModel.findOne({ _id: '5d1ead8b9b8d3b7c2dad209b' }, (err, humanvoice) => {
+    HumanVoiceModel.findOne({ title, wikiSource, lang, user: req.user._id }, (err, humanvoice) => {
       if (err) {
         console.log('error retrieving human voice', err);
         return res.status(400).send('Something went wrong');
